1818b79cfd5dd693189d1e771ba195111f47f2ec
Liana-K <lianakarim77@gmail.com>
2025-05-20 13:40:15 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,17 @@
+/*
+ * This project is inspired by A2 - TicTacToe game
+ * Color theme inspired by Charli xcx's recent 'brat' album
+ * The purpose of this implementation is to practice GUI's and associated methods with JFrame
+ * 
+ * December 2024
+ * Liana Karim
+ * 
+ */
+
+public class App {
+    public static void main(String[] args) {
+        StartScreen startScreen = new StartScreen();
+        startScreen.setVisible(true);
+        
+    }
+}
